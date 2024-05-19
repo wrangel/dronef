ee6668b016ceda640d65143a83348560951a076a
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-05-19 17:50:21 +0200

    First working grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import photo from "./assets/1.jpg";
+//import photo from "./assets/1.jpg";
+
+import React from "react";
+import Masonry from "react-masonry-css";
 import "./App.css";
 
-function App() {
-  const photos = [{ src: photo, alt: "./assets/1.jpg" }];
+const images = [
+  { src: "https://picsum.photos/id/1015/500/300", alt: "Image 1" },
+  { src: "https://picsum.photos/id/1018/500/300", alt: "Image 2" },
+  { src: "https://picsum.photos/id/1019/500/300", alt: "Image 3" },
+  { src: "https://picsum.photos/id/1020/500/300", alt: "Image 4" },
+  { src: "https://picsum.photos/id/1022/500/300", alt: "Image 5" },
+  { src: "https://picsum.photos/id/1023/500/300", alt: "Image 6" },
+  { src: "https://picsum.photos/id/1024/500/300", alt: "Image 7" },
+  { src: "https://picsum.photos/id/1025/500/300", alt: "Image 8" },
+  { src: "https://picsum.photos/id/1026/500/300", alt: "Image 9" },
+  { src: "https://picsum.photos/id/1027/500/300", alt: "Image 10" },
+];
 
+const breakpointColumnsObj = {
+  default: 4,
+  1100: 3,
+  700: 2,
+  500: 1,
+};
+
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={photos[0]} className="App-photo" alt="lala" />
-        <p>First page</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {images.map(({ src, alt }) => (
+          <div key={src} className="my-masonry-grid_item">
+            <img src={src} alt={alt} />
+          </div>
+        ))}
+      </Masonry>
     </div>
   );
 }
